56629bbd86776d52ebb828ca3b26dd6b9d9af1bd
Cristina <cristina.fontanet@josoc.cat>
2016-09-21 15:21:49 +0200

    cercle mal tancat

--- OpenGL/02-primitives/02-primitives/Circle.cpp
+++ OpenGL/02-primitives/02-primitives/Circle.cpp
@@ -1,28 +1,39 @@
 #include <GL/glew.h>
 #include <GL/glut.h>
 #include "Circle.h"
+#define _USE_MATH_DEFINES
+#include <math.h>
 
-Circle *Circle::createCircle(float x, float y, float width, float height, ShaderProgram &program)
+Circle *Circle::createCircle(float x, float y, float radius, ShaderProgram &program)
 {
-	Circle *circle = new Circle(x, y, width, height, program);
+	Circle *circle = new Circle(x, y, radius, program);
 
 	return circle;
 }
 
 
-Circle::Circle(float x, float y, float width, float height, ShaderProgram &program)
+Circle::Circle(float x, float y, float radius, ShaderProgram &program)
 {
-	float vertices[12] = { x, y, x + width, y,
-		x + width, y + width,
-		x, y, x + width, y + width,
-		x, y + width };
+
+	int i;
+							 //GLfloat radius = 0.8f; //radius
+	GLfloat twicePi = 2.0f * (GLfloat)M_PI;
+
+	float vert[triangleAmount+2];
+	vert[0] = x;
+	vert[1] = y;
+	for (i = 2; i < triangleAmount-2; i += 2) {
+		vert[i] = x + (radius * cos(i *  twicePi / triangleAmount));
+		vert[i+1] = y + (radius * sin(i * twicePi / triangleAmount));
+	}
 
 	glGenVertexArrays(1, &vao);
 	glBindVertexArray(vao);
 	glGenBuffers(1, &vbo);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glBufferData(GL_ARRAY_BUFFER, 12 * sizeof(float), vertices, GL_STATIC_DRAW);
-	posLocation = program.bindVertexAttribute("position", 2);
+	glBufferData(GL_ARRAY_BUFFER, (triangleAmount+2) * sizeof(float), vert, GL_STATIC_DRAW);
+	posLocation = program.bindVertexAttribute("position", 2); 
+
 
 
 }
@@ -31,7 +42,7 @@ void Circle::render() const
 {
 	glBindVertexArray(vao);
 	glEnableVertexAttribArray(posLocation);
-	glDrawArrays(GL_POLYGON, 0, 4);
+	glDrawArrays(GL_TRIANGLE_FAN, 0, triangleAmount+2);
 }
 
 void Circle::free()
