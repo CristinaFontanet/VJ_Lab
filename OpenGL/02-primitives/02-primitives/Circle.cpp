e89dc79f767a5e8ac2b0a0e01763a0af6a984ad2
Cristina <cristina.fontanet@josoc.cat>
2016-09-21 15:43:55 +0200

    comecocos

--- OpenGL/02-primitives/02-primitives/Circle.cpp
+++ OpenGL/02-primitives/02-primitives/Circle.cpp
@@ -14,24 +14,24 @@ Circle *Circle::createCircle(float x, float y, float radius, ShaderProgram &prog
 
 Circle::Circle(float x, float y, float radius, ShaderProgram &program)
 {
-
-	int i;
 							 //GLfloat radius = 0.8f; //radius
 	GLfloat twicePi = 2.0f * (GLfloat)M_PI;
 
-	float vert[triangleAmount+2];
+	float vert[triangleAmount*2+2];
 	vert[0] = x;
 	vert[1] = y;
-	for (i = 2; i < triangleAmount-2; i += 2) {
-		vert[i] = x + (radius * cos(i *  twicePi / triangleAmount));
-		vert[i+1] = y + (radius * sin(i * twicePi / triangleAmount));
+	int pos = 2;
+	for (int i = 0; i <= triangleAmount; i++) {
+		vert[pos] = x + (radius * cos(i *  twicePi / triangleAmount));
+		vert[pos+1] = y + (radius * sin(i * twicePi / triangleAmount));
+		pos += 2;
 	}
 
 	glGenVertexArrays(1, &vao);
 	glBindVertexArray(vao);
 	glGenBuffers(1, &vbo);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glBufferData(GL_ARRAY_BUFFER, (triangleAmount+2) * sizeof(float), vert, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, (triangleAmount*2+2) * sizeof(float), vert, GL_STATIC_DRAW);
 	posLocation = program.bindVertexAttribute("position", 2); 
 
 
@@ -42,7 +42,7 @@ void Circle::render() const
 {
 	glBindVertexArray(vao);
 	glEnableVertexAttribArray(posLocation);
-	glDrawArrays(GL_TRIANGLE_FAN, 0, triangleAmount+2);
+	glDrawArrays(GL_TRIANGLE_FAN, 0, triangleAmount);
 }
 
 void Circle::free()
